6f3d093f204fd17a5d2239ddce524ee6b4749c7f
ClaireHarned <charned@wisc.edu>
2023-03-01 16:20:42 -0600

    Initial commit

---
+++ boilerplate/js/main.js
@@ -0,0 +1,53 @@
+var map;
+
+//function to instantiate the Leaflet map
+function createMap(){
+    
+    //create the map
+    map = L.map('map', {
+        center: [30, -90],
+        zoom: 3
+    });
+
+    //add OSM base tilelayer
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(map);
+
+    //call getData function
+    getData(map);
+};
+function createPropSymbols(data){
+    //create marker options
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.6
+    };
+
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    }).addTo(map);
+};
+
+//Import GeoJSON data
+function getData(){
+    //load the data
+    fetch("data/nationalparks.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            //call function to create proportional symbols
+            createPropSymbols(json);
+            createSequenceControls();
+        })
+};
+
+document.addEventListener('DOMContentLoaded',createMap)
